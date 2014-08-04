679125483f4c55807f23c3578c5c17dca36e6429
Mike Chevett <mchevett@gmail.com>
2014-08-04 14:09:13 -0400

    grab current location

--- src/record-types/c/index.js
+++ src/record-types/c/index.js
@@ -1,13 +1,29 @@
 var $ = window.$,
 	template = require('./index.vash');
 
+
 function RecordA(){
 
 	function getCoordinateField(selector){ return +self.$element.find(selector).val(); }
 
+	function onLocationReady(data){
+		self.$element.find('.js-get-location').text('Load current location');
+		self.$element.find('.js-lng').val(data.coords.longitude);
+		self.$element.find('.js-lat').val(data.coords.latitude);
+	}
+
 	var self = this;
 	self.$element = $(template({ }));
 	self.type = 'Location';
+
+	self.$element.find('.js-get-location')
+		.on('click', function(ev){
+			ev.preventDefault();
+
+			$(this).text('Working...');
+			navigator.geolocation
+				.getCurrentPosition(onLocationReady, window.alert.bind(window, 'location error'), { enableHighAccuracy: false });
+		});
 
 	self.getData = function(){
 		return {
